b8fa80263f3f4c6fdd9a75efc0d04e2fda9ac7ef
exahn <exahn@uwaterloo.ca>
2025-01-25 16:19:20 -0500

    configured project for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'ethan118.github.io',
+    base: '/Ethan118.github.io',
+});
