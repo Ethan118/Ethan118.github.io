c71d4385577e9493fac4f661f5b49c8d887c839e
exahn <exahn@uwaterloo.ca>
2025-01-25 16:24:10 -0500

    fixed url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'ethan118.github.io',
+    site: 'https://ethan118.github.io',
 });
